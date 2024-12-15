17e8abb643935e8e2c3e72c1ae25ccd4067c949e
Rei <philippeandreid@gmail.com>
2024-12-16 04:57:18 +0800

    clickable photo on dashboard for profile

--- Frontend/app/(user_dashboard)/Dashboard.tsx
+++ Frontend/app/(user_dashboard)/Dashboard.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar";
 import { Text } from "@/components/Text";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/components/authentication/AuthContext";
+import { Button } from "@/components/Button";
 import CategoryBox from "@/components/dashboard/CategoryBox";
 import CurrentPawPal from "@/components/dashboard/CurrentPawPal";
 import RecentPawPals from "@/components/dashboard/RecentPawPals";
@@ -29,16 +30,18 @@ const Dashboard = () => {
             source={require("@/assets/images/Pawpal_Icon_Colored.svg")}
             style={styles.icon}
           />
-          <Avatar alt={"avatar"} className={"h-12 w-12"}>
-            <AvatarImage
-              source={{
-                uri: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
-              }}
-            />
-            <AvatarFallback>
-              <Text>P</Text>
-            </AvatarFallback>
-          </Avatar>
+          <Button variant={"ghost"} onPress={() => router.push("/UserProfile/UserProfileDetails")}>
+            <Avatar alt={"avatar"} className={"h-12 w-12"}>
+              <AvatarImage
+                source={{
+                  uri: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
+                }}
+              />
+              <AvatarFallback>
+                <Text>P</Text>
+              </AvatarFallback>
+            </Avatar>
+          </Button>
         </View>
         <H1 className={"py-6"}>
           Hello, {sessionStorage.getItem("firstname")}!
